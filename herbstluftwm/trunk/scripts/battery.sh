5e5216ccd673a0012df00d1000df7e3fffa22261
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:14:33 +0000

    battery megoldása daemon-nal, fcron helyett
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@44 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/battery.sh
+++ herbstluftwm/trunk/scripts/battery.sh
@@ -1,21 +1,17 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 
+PATH=${PATH}:/usr/local/bin
 pgrep -q herbstluftwm || exit 0
 
 export DISPLAY=:0
 
-LOGFILE=$HOME/logfiles/hlwm-battery
-
-source ${LOGFILE}
-
-STATUS=($(sysctl hw.acpi.battery.{life,state} | sed "s@.*: @@"))
-
-new_life=${STATUS[0]}
-new_state=${STATUS[1]}
-
-if [ $state -ne $new_state -o $life -ne $new_life ]; then
-    /usr/local/bin/herbstclient emit_hook user_battery ${new_state} ${new_life}
-    echo "state=${new_state}" > $LOGFILE
-    echo "life=${new_life}" >> $LOGFILE
-fi
+OLD_STATUS=""
+while true; do
+    STATUS=`sysctl hw.acpi.battery.state hw.acpi.battery.life | sed "s@.*: @@"`
+    if [ "${OLD_STATUS}" != "${STATUS}" ]; then
+        herbstclient emit_hook user_battery ${STATUS}
+        OLD_STATUS="${STATUS}"
+    fi
+    sleep 10
+done
 
