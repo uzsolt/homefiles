430a3b78cfb4e431f6ab5f76139b8faa5e71fab3
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:14:14 +0000

    /usr/local/bin hozzáírása
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@14 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/battery.sh
+++ herbstluftwm/trunk/scripts/battery.sh
@@ -14,7 +14,7 @@ new_life=${STATUS[0]}
 new_state=${STATUS[1]}
 
 if [ $state -ne $new_state -o $life -ne $new_life ]; then
-    herbstclient emit_hook user_battery ${new_state} ${new_life}
+    /usr/local/bin/herbstclient emit_hook user_battery ${new_state} ${new_life}
     echo "state=${new_state}" > $LOGFILE
     echo "life=${new_life}" >> $LOGFILE
 fi
