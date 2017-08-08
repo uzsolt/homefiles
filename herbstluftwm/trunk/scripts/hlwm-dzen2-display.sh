229bc34e41c91f3847351cfa10e18aa5cfec8c2a
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:15:30 +0000

    a dzen2 850-nél kezdődjön
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@135 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/hlwm-dzen2-display.sh
+++ herbstluftwm/trunk/scripts/hlwm-dzen2-display.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-~/.config/herbstluftwm/scripts/hlwm-dzen2.sh | dzen2 -x 700 -w 666 -h 18 -fn "terminus:size=8"
+~/.config/herbstluftwm/scripts/hlwm-dzen2.sh | dzen2 -x 850 -w 666 -h 18 -fn "terminus:size=8"
 
