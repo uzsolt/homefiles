98afe19efdea663665292d66a38ef1b75119b252
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:14:30 +0000

    átnevezve info-win.sh-ra és info-mpd.sh-ra
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@40 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/info_win.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PATH=${PATH}:/usr/local/bin
-
-( echo "=== Window properties ==="
-  herbstclient attr clients.focus | sed -n \
-    "/^ s/ s@[^-]*- \([^ ]*\).*=@\1 =@p"
-  sleep 5
-) | dzen2 -w 200 -l 6 -e "onstart=uncollapse"
-
