1b745387310844f29d24eccf5f28a466170e6fae
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:14:08 +0000

    milyen programok futnak, az helyett, hogy konkrét programokat keresnénk
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@5 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/hlwm_quit.sh
+++ herbstluftwm/trunk/scripts/hlwm_quit.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-APPS="firefox lowriter"
+LINES=`wmctrl -l | grep -v "tmux\|RSS" | wc -l`
+pgrep -q vim && LINES=$(( LINES+1 ))
 
-for a in ${APPS}; do
-    echo ${a}
-done
+if [ ${LINES} -gt 0 ]; then
+    (
+        echo "Herbstluftwm kilépés - futó programok"
+        wmctrl -l | grep -v "tmux\|RSS" | sed "s@.*bsd-zsolt @@"
+        pgrep -q vim && echo VIM
+        sleep 5
+    ) | dzen2 -x 800 -y 40 -w 566 -l ${LINES} -e "onstart=uncollapse" -bg "red" -fg "white"
 
+fi
