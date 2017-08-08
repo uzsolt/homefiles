68d2b64757862cfe2d3f765b66485f8f78ae8420
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:14:41 +0000

    wmname átkerült az external/autorun.sh-ba
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@59 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/external/autorun.sh
+++ herbstluftwm/trunk/external/autorun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+wmname LG3D
 rm -f /home/zsolt/passwords.kdb.lock && keepassx -min &
 conky -d -c /home/zsolt/.config/herbstluftwm/external/conkyrc
 (sleep 1 ; stalonetray -c /home/zsolt/.config/herbstluftwm/external/stalonetray) &
-
 
