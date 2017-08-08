cda182a81bac8bdcea40fd2abfca7ebab3e03918
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:14:11 +0000

    nagyon sürgős, sürgős, normál
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@10 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/taskwarrior.sh
+++ herbstluftwm/trunk/scripts/taskwarrior.sh
@@ -3,14 +3,19 @@
 get_due_task() {
     PARAM="due.before:${2}day"
     [ ${1} -gt 0 ] && PARAM="${PARAM} due.after:${1}day"
-    DT=`task ${PARAM} 2>/dev/null | tail -1 | sed "s@ task.*@@"`
+    DT=`/usr/local/bin/task ${PARAM} 2>/dev/null | tail -1 | sed "s@ task.*@@"`
     if [ -z "${DT}" ]; then
         DT=0
     fi
     echo ${DT}
 }
 
-URGENT=`get_due_task 0 5`
-LOW_URGENT=`get_due_task 5 10`
+while true; do
+    VERY_URGENT=`get_due_task 0 2`
+    URGENT=`get_due_task 2 3`
+    LOW_URGENT=`get_due_task 3 7`
+    #echo ${VERY_URGENT} ${URGENT} ${LOW_URGENT}
 
-herbstclient emit_hook user_taskwarrior ${URGENT} ${LOW_URGENT}
+    herbstclient emit_hook user_taskwarrior ${VERY_URGENT} ${URGENT} ${LOW_URGENT}
+    sleep 300
+done
