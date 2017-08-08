da990e8ce813a45ca8d1f5ad0283d4ddc5735a01
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:15:41 +0000

    musicpd playlist
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@152 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/mpd-playlist.sh
+++ herbstluftwm/trunk/scripts/mpd-playlist.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-CURRENT=$(mpc -f "%position%. [[%artist% - ]%title%]|[%file%]" | grep "^[0-9]")
-TOPLAY=$(mpc -f "%position%. [[%artist% - ]%title%]|[%file%]" playlist | dmenu -l 30 -i -p "${CURRENT}")
+song=`mpc playlist | dmenu -i -l 30 -b -nb "#000" -nf "#7af" -sb "#000" -sf "#bdf" -p "Find:" | cut -d')' -f1`
+song="`mpc playlist | awk '{if($0 == "'"${song}"'") {print NR}}'`"
 
-if [ $? -eq 0 ]; then
-    mpc play $(echo ${TOPLAY} | sed "s@^\([0-9]*\).*@\1@")
-fi
+[ -n "${song}" ] && mpc play "${song}"
 
