77e5c1461d292d139e3b15ecb87a6522442983a2
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:15:13 +0000

    freefont-os dzen-hez paraméter
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@109 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/hlwm-dzen2-display.sh
+++ herbstluftwm/trunk/scripts/hlwm-dzen2-display.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-~/.config/herbstluftwm/scripts/hlwm-dzen2.sh | dzen2 -x 700 -w 666 -h 18
+~/.config/herbstluftwm/scripts/hlwm-dzen2.sh | dzen2 -x 700 -w 666 -h 18 -fn "terminus:size=8"
 
