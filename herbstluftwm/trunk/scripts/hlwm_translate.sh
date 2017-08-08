2fa2e30abd375518838402a100b0795974d1e3f4
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:15:03 +0000

    kezdeti verzió
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@94 a4e18795-5dcb-e711-af28-005056bce12b

---
+++ herbstluftwm/trunk/scripts/hlwm_translate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+PING_HOST=8.8.8.8
+
+ret=`yad --title="Fordítás" --form --item-separator=, --field="Irány":CB "en:hu","hu:en" --field="Szöveg"`
+if [ $? -eq 0 ]; then
+    # direction
+    d=`echo ${ret} | sed 's,|.*,,'`
+    # text
+    t=`echo ${ret} | sed 's,.*|\(.*\)|,\1,'`
+    {
+        echo "net kapcsolat ellenőrzése..."
+        ping -t 3 -c 2 ${PING_HOST} > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "fordítás folyamatban..."
+            forditas=`trans -b ${d} ${t}`
+            echo "${t}: ${forditas}"
+        else
+            echo "[[ nincs net ]]"
+        fi
+        sleep 5
+    } | dzen2 -w 400 -h 20 -y 20 &
+fi
+
