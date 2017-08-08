2b8dd51f7968b209c7d46e134402a1aeb75de529
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:15:22 +0000

    TOPLAY fixálása - most már működik
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@123 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/mpd-playlist.sh
+++ herbstluftwm/trunk/scripts/mpd-playlist.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 CURRENT=$(mpc -f "%position%. [[%artist% - ]%title%]|[%file%]" | grep "^[0-9]")
-TOPLAY=$(mpc playlist | nl | sed "s@^ *\([0-9]*\)[[:space:]]*@\1 @" | dmenu -l 30 -i -p "${CURRENT}")
+TOPLAY=$(mpc -f "%position%. [[%artist% - ]%title%]|[%file%]" playlist | dmenu -l 30 -i -p "${CURRENT}")
 
 if [ $? -eq 0 ]; then
-    #echo ${TOPLAY}
-    mpc play $(echo ${TOPLAY} | sed "s@\([0-9]*\).*@\1@")
+    mpc play $(echo ${TOPLAY} | sed "s@^\([0-9]*\).*@\1@")
 fi
 
