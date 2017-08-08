f43f49b4b1f19e7a5ed3a8496b2f9f9a58064c59
zsolt <zsolt@a4e18795-5dcb-e711-af28-005056bce12b>
2017-08-08 10:14:33 +0000

    swap megvalósítása daemon-nal
    git-svn-id: svn+ssh://svn.uzsolt.hu/usr/local/subversion/homefiles@45 a4e18795-5dcb-e711-af28-005056bce12b

--- herbstluftwm/trunk/scripts/swap.sh
+++ herbstluftwm/trunk/scripts/swap.sh
@@ -1,16 +1,17 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 
 PATH=${PATH}:/usr/local/bin
 pgrep -q herbstluftwm || exit 0
-
 export DISPLAY=:0
-LOGFILE=$HOME/logfiles/hlwm-swap
 
-source ${LOGFILE}
-STATUS=`swapinfo | sed -rn "$ s@.* ([^ ]*)%@\1@p"`
+OLD_STATUS=""
+while true; do
+    STATUS=`swapinfo | sed -rn "$ s@.* ([^ ]*)%@\1@p"`
+    if [ "${STATUS}" != "${OLD_STATUS}" ]; then
+        herbstclient emit_hook user_swap ${STATUS}
+        OLD_STATUS=${STATUS}
+    fi
+    sleep 10
+done
 
-if [ ${STATUS} -ne ${OLD_STATUS} ]; then
-    herbstclient emit_hook user_swap ${STATUS}
-    echo "OLD_STATUS=${STATUS}" > ${LOGFILE}
-fi
 
